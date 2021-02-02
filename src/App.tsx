581802020c3fe37cbd929cc9e60e0ef0a7857da9
Marley Anthony <62618323+marleyanthony@users.noreply.github.com>
2021-02-01 19:17:24 -0800

    added hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './Styles/main.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './Components/Header';
+import Home from './Components/Pages/Home';
 
 function App() {
    return (
       <div className="App">
          <Router>
             <Header />
+            <Route path="/" component={Home} />
          </Router>
       </div>
    );
---
+++ src/Components/Pages/Home.tsx
@@ -0,0 +1,11 @@
+import Hero from '../Hero';
+
+const Home = () => {
+   return (
+      <div className="home-page">
+         <Hero />
+      </div>
+   )
+}
+
+export default Home
