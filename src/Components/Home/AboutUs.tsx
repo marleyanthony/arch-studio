51bcb2fed14ff0eea48ebf3dc306f46abdb94066
Marley Anthony <62618323+marleyanthony@users.noreply.github.com>
2021-02-06 18:04:07 -0800

    mobile styles for about us section

--- src/Components/Home/AboutUs.tsx
+++ src/Components/Home/AboutUs.tsx
@@ -1,17 +1,19 @@
 import { Link } from 'react-router-dom'
 const AboutUs = () => {
-   return (
-      <section className="about-us">
-         <div className="about-us__overlay"></div>
-         <h2 className="about-us__heading">
-            Small team, big ideas
+  return (
+    <section className="about-us">
+      <div className="about-us__overlay"></div>
+      <div className="about-us__content-wrapper">
+        <h2 className="about-us__heading">
+          Small team, big ideas
          </h2>
-         <Link to='/about-us'>
-            About Us
+        <Link to='/about-us' className="about-us__button">
+          About Us
             <i className="fas fa-arrow-right hero__arrow"></i>
-         </Link>
-      </section>
-   )
+        </Link>
+      </div>
+    </section>
+  )
 }
 
 export default AboutUs
